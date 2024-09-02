d25e3a556346f57ea78773ee5b5727030c4b704f
Michael St Clair <michaelst57@gmail.com>
2024-09-01 19:22:37 -0600

    cleanup

--- app/lib/uptime/env.ex
+++ app/lib/uptime/env.ex
@@ -4,11 +4,4 @@ defmodule Uptime.Env do
   """
   @spec build_version() :: String.t()
   def build_version, do: Application.get_env(:uptime, :build_version)
-
-  @spec request_tracer_connection() :: String.t()
-  def request_tracer_connection do
-    host = System.get_env("REQUEST_TRACER_HOST") || "localhost"
-    port = String.to_integer(System.get_env("REQUEST_TRACER_PORT") || "50051")
-    "#{host}:#{port}"
-  end
 end
