cc81e493787e92cfa381466381a39c43a34cc23e
Michael St Clair <michaelst57@gmail.com>
2024-09-04 22:00:49 -0600

    env tweaks (#16)

--- lib/uptime/env.ex
+++ lib/uptime/env.ex
@@ -31,6 +31,16 @@ defmodule Uptime.Env do
     :ok
   end
 
+  @spec has?(String.t()) :: boolean()
+  def has?(name) do
+    name
+    |> read()
+    |> valid_env_var_value()
+  end
+
+  @spec get(atom(), any()) :: any()
+  def get(key, default \\ nil), do: Application.get_env(:uptime, key, default)
+
   @doc """
   Read a value from the environment.
   First check for a secrets file, then the environment, then the config file.
@@ -43,9 +53,8 @@ defmodule Uptime.Env do
     secret_file_path = Path.join(@secrets_directory, prefixed_name)
 
     with {:error, _error} <- File.read(secret_file_path),
-         :error <- read_from_env(prefixed_name),
-         :error <-
-           read_from_config_file(name, find_config_path()) do
+         :error <- System.fetch_env(prefixed_name),
+         :error <- read_from_config_file(name) do
       default
     else
       {:ok, value} ->
@@ -57,52 +66,27 @@ defmodule Uptime.Env do
     end
   end
 
-  @spec read_from_env(String.t()) :: {:ok, String.t()} | :error
-  defp read_from_env(name) do
-    value = System.get_env(name)
-
-    if valid_env_var_value(value) do
-      {:ok, value}
-    else
-      :error
-    end
-  end
-
-  @spec read_from_config_file(String.t(), String.t()) :: any() | nil
-  defp read_from_config_file(key, path) when is_binary(path) do
+  @spec read_from_config_file(String.t()) :: any() | nil
+  defp read_from_config_file(key) do
     key = String.downcase(key)
 
-    case YamlElixir.read_from_file(path) do
-      {:ok, config} ->
-        # TODO: Implement a deep get function when we add nested configuration structure
-        {:ok, config[key]}
-
-      _not_found ->
+    with path when is_binary(path) <- find_config_path(),
+         {:ok, config} <- YamlElixir.read_from_file(path) do
+      {:ok, config[key]}
+    else
+      _error ->
         :error
     end
   end
-
-  defp read_from_config_file(_key, nil), do: :error
 
   @spec find_config_path() :: String.t() | nil
   defp find_config_path do
     @supported_config_filenames
     |> Enum.reduce([], fn file, acc ->
-      acc ++
-        Enum.map(get(:supported_config_dirs, ["/etc/uptimes/config.yaml"]), &Path.join(&1, file))
+      Enum.map(get(:supported_config_dirs, ["/etc/uptimes/config.yaml"]), &Path.join(&1, file)) ++ acc
     end)
     |> Enum.find(&File.exists?/1)
   end
-
-  @spec has?(String.t()) :: boolean()
-  def has?(name) do
-    name
-    |> read()
-    |> valid_env_var_value()
-  end
-
-  @spec get(atom(), any()) :: any()
-  def get(key, default \\ nil), do: Application.get_env(:uptime, key, default)
 
   @spec valid_env_var_value(String.t()) :: boolean()
   defp valid_env_var_value(value) do
