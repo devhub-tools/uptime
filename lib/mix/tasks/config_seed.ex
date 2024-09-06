74e8141c5970cd053bbb24be1d80601ae993aca2
brett <brettgardiner@gmail.com>
2024-09-06 12:13:27 -0600

    check for existing service before creating check job

--- lib/mix/tasks/config_seed.ex
+++ lib/mix/tasks/config_seed.ex
@@ -5,6 +5,8 @@ defmodule Mix.Tasks.ConfigSeed do
 
   use Mix.Task
 
+  import Ecto.Query
+
   alias Uptime.Repo
 
   require Logger
@@ -14,10 +16,11 @@ defmodule Mix.Tasks.ConfigSeed do
   @impl Mix.Task
   def run(_args) do
     load()
+    existing_ids = Repo.all(from(s in Uptime.Service, select: s.id))
 
     :services
     |> Uptime.Env.get()
-    |> Enum.map(&save_service/1)
+    |> Enum.map(&save_service(&1, existing_ids))
     |> Enum.filter(fn {result, _} -> result == :error end)
     |> Enum.map(fn {_result, changeset} -> changeset_error_to_string(changeset) end)
     |> Enum.each(&Logger.error/1)
@@ -29,22 +32,30 @@ defmodule Mix.Tasks.ConfigSeed do
     Repo.start_link(pool_size: 2)
   end
 
-  defp save_service(attrs) do
+  defp save_service(attrs, existing_ids) do
     attrs
     |> Uptime.Service.changeset()
     |> Repo.insert(
       on_conflict: {:replace_all_except, [:id, :inserted_at]},
       conflict_target: :slug,
-      returning: false
+      returning: true
     )
-    |> tap(&create_job/1)
+    |> tap(fn
+      {:ok, service} ->
+        unless Enum.member?(existing_ids, service.id) do
+          create_job(service.id)
+        end
+
+      {:error, _changeset} ->
+        nil
+    end)
   end
 
-  defp create_job({:ok, service}) do
-    %{id: service.id} |> Uptime.CheckJob.new(scheduled_at: DateTime.utc_now()) |> Repo.insert()
+  defp create_job(service_id) when is_binary(service_id) do
+    %{id: service_id}
+    |> Uptime.CheckJob.new(scheduled_at: DateTime.utc_now())
+    |> Repo.insert()
   end
-
-  defp create_job({:error, _} = result), do: result
 
   defp changeset_error_to_string(changeset) do
     changeset
